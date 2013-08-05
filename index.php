ce8a965198f69d103978d8cba81290e11d944123
Amol Chawathe <amol.chawathe@rocketunder.com>
2013-08-05 16:14:16 +0800

    This is a new test

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
  * and open the template in the editor.
  */
 
-echo "This is a test" ;
+echo "This is a test   amol is also working" ;
 echo "This is just a simple disconnect" ;
 echo "Shahid joining the phing team" ;
 echo "This is just a simple disconnect part 2 " ;
 
-echo "this is shahid work";
+echo "this is shahid work and amol work";
 
 echo phpinfo();
 
