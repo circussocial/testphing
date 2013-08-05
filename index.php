47f27a685a1d0bb9923531c3f6c435d99148f3c6
Mohdshahid <Mohdshahid@192.168.1.2>
2013-08-05 13:40:49 +0530

    more work

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
  * and open the template in the editor.
  */
 
-echo "This is a test" ;
+echo "This is a test and test again by shahid" ;
 echo "This is just a simple disconnect" ;
 echo "Shahid joining the phing team" ;
 echo "This is just a simple disconnect part 2 " ;
 
-echo "this is shahid work";
+echo "this is shahid work and more work";
 
 echo phpinfo();
 
